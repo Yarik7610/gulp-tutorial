b692d661da3f263623178a3a70281ab7f5c83894
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-26 16:32:07 +0300

    Remove decomposition for gulpfile config
    The problem occurs because i need one browserSync one instance for all
    tasks, but even passing it as an argument to task function doesn't work

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const { watch, parallel } = require("gulp")
+const browserSync = require("browser-sync").create()
+
+const { src, dest } = require("gulp")
+const concat = require("gulp-concat") //union all files into one file + rename (use gulp-rename for only renaming)
+
+const scss = require("gulp-sass")(require("sass"))
+const autoprefixer = require("gulp-autoprefixer")
+
+const uglify = require("gulp-uglify-es").default //minificate js-code, like scss({outputStyle: "compressed"})
+
+function styles() {
+  return src("src/scss/style.scss")
+    .pipe(concat("style.min.css"))
+    .pipe(autoprefixer())
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(dest("src/css"))
+    .pipe(browserSync.stream()) //.stream == change without refreshing the page
+}
+
+function scripts() {
+  return src("src/js/main.js")
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("src/js"))
+    .pipe(browserSync.stream())
+}
+
+function watching() {
+  watch(["src/scss/style.scss"], styles)
+  watch(["src/js/main.js"], scripts)
+  watch(["src/*.html"]).on("change", browserSync.reload) //.reload == full page reload
+}
+
+function browsersync() {
+  //make static dev server
+  browserSync.init({
+    server: {
+      baseDir: "src/"
+    }
+  })
+}
+
+exports.styles = styles //gulp styles in terminal
+exports.scripts = scripts
+exports.watching = watching
+exports.browsersync = browsersync
+
+exports.default = parallel(styles, scripts, browsersync, watching)
