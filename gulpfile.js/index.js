09f4000cbaa925cabc0e4e190854522db73e5ef9
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-26 13:26:49 +0300

    Add gulp watch mode

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,5 +1,12 @@
 const { styles } = require("./styles")
 const { scripts } = require("./scripts")
+const { watch } = require("gulp")
 
-exports.styles = styles
+function watching() {
+  watch(["src/scss/style.css"], styles)
+  watch(["src/js/main.js"], scripts)
+}
+
+exports.styles = styles //gulp styles in terminal
 exports.scripts = scripts
+exports.watching = watching
