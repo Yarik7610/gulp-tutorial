c54c1b90d6d2342bf7dc5afa3430541e8435e81e
Yaroslav Yanovich <arikarik782@gmail.com>
2024-10-26 13:55:42 +0300

    Add browsersync

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,12 +1,25 @@
 const { styles } = require("./styles")
 const { scripts } = require("./scripts")
+
 const { watch } = require("gulp")
+const browserSync = require("browser-sync").create()
 
 function watching() {
   watch(["src/scss/style.css"], styles)
   watch(["src/js/main.js"], scripts)
+  watch(["src/*.html"]).on("change", browserSync.reload) //.reload == full page reload
+}
+
+function browsersync() {
+  //make static dev server
+  browserSync.init({
+    server: {
+      baseDir: "src/"
+    }
+  })
 }
 
 exports.styles = styles //gulp styles in terminal
 exports.scripts = scripts
 exports.watching = watching
+exports.browsersync = browsersync
